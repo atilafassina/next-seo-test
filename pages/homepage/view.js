425f2fe598ad12e89fbf9467acb0ea5660828999
Atila Fassina <hey@atilafassina.com>
2017-02-13 17:34:59 +0100

    hide giphy

--- pages/homepage/view.js
+++ pages/homepage/view.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 import Navigation from '../../components/Navigation/container'
-import Giphy from '../../components/Footer/container'
+// import Giphy from '../../components/Footer/container'
 // import Offer from '../../components/Offer/container'
-
+//<Giphy />
 export default class PageView extends Component {
   render(props) {
     return (
       <div>
         <Navigation />
-        <Giphy />
+
     </div>)
   }
 }
