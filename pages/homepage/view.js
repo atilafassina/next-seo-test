d58efe0c2e297a1ea55e6c9deb0aef184a5d31af
Atila Fassina <hey@atilafassina.com>
2017-02-13 18:09:39 +0100

    add text to the homepage

--- pages/homepage/view.js
+++ pages/homepage/view.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
 import Navigation from '../../components/Navigation/container'
-// import Giphy from '../../components/Footer/container'
+import Giphy from '../../components/Footer/container'
 // import Offer from '../../components/Offer/container'
-//<Giphy />
 export default class PageView extends Component {
   render(props) {
     return (
       <div>
         <Navigation />
-
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Maxime debitis dolorem sapiente, assumenda, libero tempora a, perferendis nemo error architecto ipsam. Veniam ipsam praesentium debitis nemo animi consectetur fugiat, labore qui delectus. Omnis enim harum temporibus earum eveniet similique veniam et! Tempora sapiente quisquam, tenetur aspernatur minus repudiandae illo, corporis!
+        </p>
+        <Giphy />
     </div>)
   }
 }
